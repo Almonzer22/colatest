ac4d1a30cd9855019a007b035d61be77f7352693
aya.mouner <aya.mouner@DFI-AYA-MON>
2023-01-10 15:58:15 +0200

    Testing

---
+++ WebApplication1/WebApplication1/Controllers/AreasController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ColaProject.Data;
+using ColaProject.Models;
+
+namespace ColaProject.Controllers
+{
+    public class AreasController : Controller
+    {
+        private readonly BusStationSystemContext _context;
+
+        public AreasController(BusStationSystemContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Areas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Areas.ToListAsync());
+        }
+
+        // GET: Areas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var areas = await _context.Areas
+                .FirstOrDefaultAsync(m => m.AreaId == id);
+            if (areas == null)
+            {
+                return NotFound();
+            }
+
+            return View(areas);
+        }
+
+        // GET: Areas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Areas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AreaId,AreaName")] Areas areas)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(areas);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(areas);
+        }
+
+        // GET: Areas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var areas = await _context.Areas.FindAsync(id);
+            if (areas == null)
+            {
+                return NotFound();
+            }
+            return View(areas);
+        }
+
+        // POST: Areas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AreaId,AreaName")] Areas areas)
+        {
+            if (id != areas.AreaId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(areas);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AreasExists(areas.AreaId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(areas);
+        }
+
+        // GET: Areas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var areas = await _context.Areas
+                .FirstOrDefaultAsync(m => m.AreaId == id);
+            if (areas == null)
+            {
+                return NotFound();
+            }
+
+            return View(areas);
+        }
+
+        // POST: Areas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var areas = await _context.Areas.FindAsync(id);
+            _context.Areas.Remove(areas);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AreasExists(int id)
+        {
+            return _context.Areas.Any(e => e.AreaId == id);
+        }
+    }
+}
