613eb178e4978ff2acc3a981c2245cb7b53c301d
hadeel.khogali <hadeel.khogali@APR-ICT-14>
2023-01-19 11:01:57 +0200

    Data Table

---
+++ WebApplication1/WebApplication1/Controllers/OperatorsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ColaProject.Data;
+using ColaProject.Models;
+
+namespace ColaProject.Controllers
+{
+    public class OperatorsController : Controller
+    {
+        private readonly BusStationSystemContext _context;
+
+        public OperatorsController(BusStationSystemContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Operators
+        public async Task<IActionResult> Index()
+        {
+            var busStationSystemContext = _context.Operators.Include(o => o.CreatedByNavigation).Include(o => o.UpdatedByNavigation);
+            return View(await busStationSystemContext.ToListAsync());
+        }
+
+        // GET: Operators/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var operators = await _context.Operators
+                .Include(o => o.CreatedByNavigation)
+                .Include(o => o.UpdatedByNavigation)
+                .FirstOrDefaultAsync(m => m.OperatorId == id);
+            if (operators == null)
+            {
+                return NotFound();
+            }
+
+            return View(operators);
+        }
+
+        // GET: Operators/Create
+        public IActionResult Create()
+        {
+            ViewData["CreatedBy"] = new SelectList(_context.Users, "UserId", "Name");
+            ViewData["UpdatedBy"] = new SelectList(_context.Users, "UserId", "Name");
+            return View();
+        }
+
+        // POST: Operators/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("OperatorId,OperatorName,Address,OperatorStatus,Disablity,CreatedBy,UpdatedBy,CreateDate,UpdateDate")] Operators operators)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(operators);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatedBy"] = new SelectList(_context.Users, "UserId", "Name", operators.CreatedBy);
+            ViewData["UpdatedBy"] = new SelectList(_context.Users, "UserId", "Name", operators.UpdatedBy);
+            return View(operators);
+        }
+
+        // GET: Operators/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var operators = await _context.Operators.FindAsync(id);
+            if (operators == null)
+            {
+                return NotFound();
+            }
+            ViewData["CreatedBy"] = new SelectList(_context.Users, "UserId", "Name", operators.CreatedBy);
+            ViewData["UpdatedBy"] = new SelectList(_context.Users, "UserId", "Name", operators.UpdatedBy);
+            return View(operators);
+        }
+
+        // POST: Operators/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("OperatorId,OperatorName,Address,OperatorStatus,Disablity,CreatedBy,UpdatedBy,CreateDate,UpdateDate")] Operators operators)
+        {
+            if (id != operators.OperatorId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(operators);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OperatorsExists(operators.OperatorId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatedBy"] = new SelectList(_context.Users, "UserId", "Name", operators.CreatedBy);
+            ViewData["UpdatedBy"] = new SelectList(_context.Users, "UserId", "Name", operators.UpdatedBy);
+            return View(operators);
+        }
+
+        // GET: Operators/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var operators = await _context.Operators
+                .Include(o => o.CreatedByNavigation)
+                .Include(o => o.UpdatedByNavigation)
+                .FirstOrDefaultAsync(m => m.OperatorId == id);
+            if (operators == null)
+            {
+                return NotFound();
+            }
+
+            return View(operators);
+        }
+
+        // POST: Operators/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var operators = await _context.Operators.FindAsync(id);
+            _context.Operators.Remove(operators);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OperatorsExists(int id)
+        {
+            return _context.Operators.Any(e => e.OperatorId == id);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/SupervisersController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ColaProject.Data;
+using ColaProject.Models;
+
+namespace ColaProject.Controllers
+{
+    public class SupervisersController : Controller
+    {
+        private readonly BusStationSystemContext _context;
+
+        public SupervisersController(BusStationSystemContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Supervisers
+        public async Task<IActionResult> Index()
+        {
+            var busStationSystemContext = _context.Supervisers.Include(s => s.CreatedByNavigation).Include(s => s.UpdatedByNavigation);
+            return View(await busStationSystemContext.ToListAsync());
+        }
+
+        // GET: Supervisers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var supervisers = await _context.Supervisers
+                .Include(s => s.CreatedByNavigation)
+                .Include(s => s.UpdatedByNavigation)
+                .FirstOrDefaultAsync(m => m.SuperviserId == id);
+            if (supervisers == null)
+            {
+                return NotFound();
+            }
+
+            return View(supervisers);
+        }
+
+        // GET: Supervisers/Create
+        public IActionResult Create()
+        {
+            ViewData["CreatedBy"] = new SelectList(_context.Users, "UserId", "Name");
+            ViewData["UpdatedBy"] = new SelectList(_context.Users, "UserId", "Name");
+            return View();
+        }
+
+        // POST: Supervisers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SuperviserId,SuperviserName,UserName,UserPassword,SuperviserStatus,CreatedBy,CreatedDate,UpdatedBy,UpdatedDate")] Supervisers supervisers)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(supervisers);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatedBy"] = new SelectList(_context.Users, "UserId", "Name", supervisers.CreatedBy);
+            ViewData["UpdatedBy"] = new SelectList(_context.Users, "UserId", "Name", supervisers.UpdatedBy);
+            return View(supervisers);
+        }
+
+        // GET: Supervisers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var supervisers = await _context.Supervisers.FindAsync(id);
+            if (supervisers == null)
+            {
+                return NotFound();
+            }
+            ViewData["CreatedBy"] = new SelectList(_context.Users, "UserId", "Name", supervisers.CreatedBy);
+            ViewData["UpdatedBy"] = new SelectList(_context.Users, "UserId", "Name", supervisers.UpdatedBy);
+            return View(supervisers);
+        }
+
+        // POST: Supervisers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SuperviserId,SuperviserName,UserName,UserPassword,SuperviserStatus,CreatedBy,CreatedDate,UpdatedBy,UpdatedDate")] Supervisers supervisers)
+        {
+            if (id != supervisers.SuperviserId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(supervisers);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SupervisersExists(supervisers.SuperviserId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatedBy"] = new SelectList(_context.Users, "UserId", "Name", supervisers.CreatedBy);
+            ViewData["UpdatedBy"] = new SelectList(_context.Users, "UserId", "Name", supervisers.UpdatedBy);
+            return View(supervisers);
+        }
+
+        // GET: Supervisers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var supervisers = await _context.Supervisers
+                .Include(s => s.CreatedByNavigation)
+                .Include(s => s.UpdatedByNavigation)
+                .FirstOrDefaultAsync(m => m.SuperviserId == id);
+            if (supervisers == null)
+            {
+                return NotFound();
+            }
+
+            return View(supervisers);
+        }
+
+        // POST: Supervisers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var supervisers = await _context.Supervisers.FindAsync(id);
+            _context.Supervisers.Remove(supervisers);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SupervisersExists(int id)
+        {
+            return _context.Supervisers.Any(e => e.SuperviserId == id);
+        }
+    }
+}
